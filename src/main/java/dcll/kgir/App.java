aa7c2f00c754fb14743aa6c3ef5f31d5149a5638
Girox64 <kevingiraud64@gmail.com>
2016-03-16 17:46:16 +0100

    Modification calculateur car erreur

--- src/main/java/dcll/kgir/App.java
+++ src/main/java/dcll/kgir/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Calculateur c = new Calculateur("5/5/5/5/5/5/5/5/5/5/5");
+        int s = c.calculScore();
+        System.out.println(""+s);
     }
 }
